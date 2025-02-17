865c3d2c7400d2ac8de6d55123c4a47d73c4512d
Jorge-Garrido <135667583+Jorge-Garrido@users.noreply.github.com>
2025-02-17 10:53:02 -0500

    Delete train_secundary.py

--- train_secundary.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt") 
-
-# Entrenar el modelo con el dataset personalizado
-model.train(data="/content/gdrive/MyDrive/entrenamiento_deteccion_secundario/data_secundary.yaml", epochs=200, batch=16)
-model.save("/content/gdrive/MyDrive/entrenamiento_deteccion_secundario/modelo_palabras_checks.pt")
